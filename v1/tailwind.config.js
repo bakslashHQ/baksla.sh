ca36aa9427090bc639cfa222617ced6da76b026f
Mathias Arlaud <mathias.arlaud@gmail.com>
2024-12-24 12:56:16 +0100

    chore: install symfony

---
+++ v1/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      'index.html',
+      'legal-notices.html',
+      './assets/js/index.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'Poppins Fallback'],
+        'handwritten': ['Kalam', 'Kalam Fallback'],
+      },
+      backgroundImage: {
+        'code-img': "url('../image/code.png')",
+      },
+    },
+  },
+}
