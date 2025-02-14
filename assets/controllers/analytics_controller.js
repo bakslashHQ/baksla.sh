f7077b1b43f854c693d7af0462348f44d60a637c
Mathias Arlaud <mathias.arlaud@gmail.com>
2025-02-14 16:57:45 +0100

    fix analytics

---
+++ assets/controllers/analytics_controller.js
@@ -0,0 +1,27 @@
+import googleAnalytics from '@analytics/google-analytics';
+import { Controller } from '@hotwired/stimulus';
+import Analytics from 'analytics';
+
+/**
+ * @property {String} appNameValue
+ * @property {String} measurementIdValue
+ */
+export default class extends Controller {
+  static values = {
+    appName: String,
+    measurementId: String,
+  };
+
+  connect() {
+    const analytics = Analytics({
+      app: this.appNameValue,
+      plugins: [
+        googleAnalytics({
+          measurementIds: [this.measurementIdValue],
+        }),
+      ],
+    });
+
+    analytics.page();
+  }
+}
