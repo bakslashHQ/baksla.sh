be9762e1d9060a68ea68a29704aa7b712b1a7c41
Mathias Arlaud <mathias.arlaud@gmail.com>
2025-03-12 17:54:37 +0100

    fix: remove useless hash

--- src/Blog/Domain/Model/ArticlePreview.php
+++ src/Blog/Domain/Model/ArticlePreview.php
@@ -6,14 +6,11 @@ namespace App\Blog\Domain\Model;
 
 final readonly class ArticlePreview
 {
-    public string $hash;
-
     public function __construct(
         public string $id,
         public string $title,
         public string $description,
         public Author $author,
     ) {
-        $this->hash = hash('xxh128', json_encode($this) ?: '');
     }
 }
