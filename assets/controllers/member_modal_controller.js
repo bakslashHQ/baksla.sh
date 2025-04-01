10182a94dc38344092ef0dc4e95e380a0d56f5b8
Mathias Arlaud <mathias.arlaud@gmail.com>
2025-04-01 11:24:30 +0200

    [Team] Fix scroll and bio

--- assets/controllers/member_modal_controller.js
+++ assets/controllers/member_modal_controller.js
@@ -28,7 +28,7 @@ export default class extends Controller {
   }
 
   open() {
-    document.body.classList.add('overflow-y-hidden');
+    document.body.classList.add('fixed', 'overflow-y-hidden');
 
     this.element.classList.remove('hidden');
 
@@ -42,7 +42,7 @@ export default class extends Controller {
   }
 
   close() {
-    document.body.classList.remove('overflow-y-hidden');
+    document.body.classList.remove('fixed', 'overflow-y-hidden');
 
     this.timeoutClose = setTimeout(() => {
       this.element.classList.add('hidden');
