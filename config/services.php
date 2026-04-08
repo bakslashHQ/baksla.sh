03f9dd5897852531d47fb41dbf3300b7327dbc70
Hugo Alliaume <hugo@alliau.me>
2026-04-08 11:47:16 +0200

    [Blog] Webpack Encore: What's New 8 Months Later? (#105)

--- config/services.php
+++ config/services.php
@@ -12,7 +12,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $params
         ->set('app.public_dir', sprintf('%s/public', param('kernel.project_dir')))
-        ->set('app.showcased_article', 'webpack-encore-current-state-and-future') // The filename without the ".md.twig" extension
+        ->set('app.showcased_article', 'webpack-encore-whats-new-8-months-later') // The filename without the ".md.twig" extension
         ->set('app.articles_dir', sprintf('%s/templates/articles', param('kernel.project_dir')))
         ->set('app.ssg_output_dir', sprintf('%s/static-pages', param('app.public_dir')));
 
