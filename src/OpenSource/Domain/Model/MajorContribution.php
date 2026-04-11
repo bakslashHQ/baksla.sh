1dbdd9dcfbb15e4a90a837b597ee28e55e0f1570
Hugo Alliaume <hugo@alliau.me>
2026-04-11 10:03:08 +0200

    [Contribution] Display project url/icon

--- src/OpenSource/Domain/Model/MajorContribution.php
+++ src/OpenSource/Domain/Model/MajorContribution.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\OpenSource\Domain\Model;
 
 use App\Team\Domain\Model\MemberId;
+use function Symfony\Component\String\s;
 
 final readonly class MajorContribution
 {
@@ -15,5 +16,13 @@ final readonly class MajorContribution
         public Project $project,
         public MemberId $creator,
     ) {
+    }
+
+    public function getShortUrl(): string
+    {
+        return s($this->url)
+            ->replace('https://github.com/', '')
+            ->replace('/pull/', '#')
+            ->toString();
     }
 }
